080a4d51e0b51c880392dff6c46b55f2e6d8042f
etet00 <savesola@gmail.com>
2021-08-31 22:57:55 +0800

    fix db_wrapper.py

--- db_wrapper.py
+++ db_wrapper.py
@@ -3,15 +3,16 @@ from setting import DB_PASSWORD
 from pymongo import MongoClient
 # from pprint import pprint
 
-CONNECTION = "mongodb+srv://{}:{}@cluster0.f5nmm.mongodb.net/post?retryWrites=true&w=majority".format(
-    DB_USERNAME, DB_PASSWORD)
+# CONNECTION = "mongodb+srv://{}:{}@cluster0.f5nmm.mongodb.net/post?retryWrites=true&w=majority".format(
+#     DB_USERNAME, DB_PASSWORD)
 
 # CONNECTION = "mongodb+srv://yoshi:Jx2rbQXpORMX3D6U@cluster0.wf5gy.mongodb.net/air?retryWrites=true&w=majority"
 
 
 class DBWrapper:
     def __init__(self):
-        self.client = MongoClient(CONNECTION)
+        self.client = MongoClient(
+            "mongodb+srv://yoshi:Jx2rbQXpORMX3D6U@cluster0.wf5gy.mongodb.net/air?retryWrites=true&w=majority")
         self.db = self.client.face_book_crawler
 
     def get_data(self, site):
